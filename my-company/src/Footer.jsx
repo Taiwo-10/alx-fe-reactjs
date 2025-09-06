951bd443f892771927baf97c6a41e45f95477525
Taiwo-10 <taiwooluwafemi654@gmail.com>
2025-09-06 09:51:14 +0100

    Add files via upload

---
+++ my-company/src/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        marginTop: "20px",
+        padding: "10px",
+        backgroundColor: "#333",
+        color: "white",
+        textAlign: "center"
+      }}
+    >
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
