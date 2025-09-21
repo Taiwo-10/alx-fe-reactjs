cac1b9a6d402901cafb91f267738bec07a07d806
Taiwo <taiwooluwafemi654@gmail.com>
2025-09-21 21:29:53 +0100

    Initial commit with project files

---
+++ src/services/githubService.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    return response.data;
+  } catch (error) {
+    throw new Error("Error fetching user data");
+  }
+};
+
+
+export const fetchAdvancedUsers = async (username, location, minRepos) => {
+  try {
+    let query = "";
+
+    if (username) query += `${username} `;
+    if (location) query += `location:${location} `;
+    if (minRepos) query += `repos:>=${minRepos}`;
+
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${encodeURIComponent(query)}`
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error("Error fetching users");
+  }
+};
