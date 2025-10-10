b3a744565be593b40f24e87409e2fab85e38ccb3
Taiwo-10 <taiwooluwafemi654@gmail.com>
2025-10-10 03:52:09 +0100

    Add files via upload

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,14 @@
+import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/FormikForm";
+
+
+
+function App() {
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
