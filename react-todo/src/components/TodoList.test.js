102c12bb66ac71d8fe53412fcb804da3e28a3a7b
Taiwo-10 <taiwooluwafemi654@gmail.com>
2025-10-12 22:13:25 +0100

    Add files via upload

---
+++ react-todo/src/components/TodoList.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../components/TodoList";
+
+describe("TodoList Component", () => {
+  test("renders initial todos", () => {
+    render(<TodoList />);
+    expect(screen.getByText("Learn React")).toBeInTheDocument();
+    expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
+  });
+
+  test("adds a new todo", () => {
+    render(<TodoList />);
+    const input = screen.getByTestId("todo-input");
+    const button = screen.getByTestId("add-button");
+
+    fireEvent.change(input, { target: { value: "Write Tests" } });
+    fireEvent.click(button);
+
+    expect(screen.getByText("Write Tests")).toBeInTheDocument();
+  });
+
+  test("toggles todo completion", () => {
+    render(<TodoList />);
+    const todo = screen.getByText("Learn React");
+    fireEvent.click(todo);
+    expect(todo).toHaveStyle("text-decoration: line-through");
+  });
+
+  test("deletes a todo", () => {
+    render(<TodoList />);
+    const deleteButtons = screen.getAllByTestId("delete-button");
+    fireEvent.click(deleteButtons[0]); // delete first todo
+    expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+  });
+});
